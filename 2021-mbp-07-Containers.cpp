a12dbef9e984df3bf6d2161456cdbf7dd10e1888
mvatanasov18 <mvatanasov18@codingburgas.bg>
2022-02-24 09:15:14 +0200

    Add contains

--- 2021-mbp-07-Containers.cpp
+++ 2021-mbp-07-Containers.cpp
@@ -6,35 +6,45 @@ using namespace std;
 
 void display(set<string> set1)
 {
-    for (string s : set1)
-    {
-        cout << s << " ";
-    }
+	for (string s : set1)
+	{
+		cout << s << " ";
+	}
+}
+
+bool contains(set<string> setTocheck, string value)
+{
+	for (string hui : setTocheck)
+	{
+		if (hui == value)
+		{
+			return true;
+		}
+	}
+	return false;
 }
 
 void unionWith(set<string> set1, set<string>set2)
 {
-    set<string> temp;
-    for (set<string>::iterator it = set1.begin(); it != set1.end(); it++)
-    {
-        temp.insert(*it);
-    }
-    for (set<string>::iterator it = set2.begin(); it != set2.end(); it++)
-    {
-        if (temp.find(*it) == temp.end())
-        {
-            temp.insert(*it);
-        }
-    }
-    display(temp);
+	set<string> temp;
+	for (set<string>::iterator it = set1.begin(); it != set1.end(); it++)
+	{
+		temp.insert(*it);
+	}
+	for (set<string>::iterator it = set2.begin(); it != set2.end(); it++)
+	{
+		temp.insert(*it);
+
+	}
+	display(temp);
 }
 
 int main()
 {
-    set<string>set1 = {"Ivan","Kaloqn","Kiril","Mario"};
-    set<string>set2 = { "Martin", "Mario", "Petar","Kiril" };
-    unionWith(set1, set2);
+	set<string>set1 = { "Ivan","Kaloqn","Kiril","Mario" };
+	set<string>set2 = { "Martin", "Mario", "Petar","Kiril" };
+	unionWith(set1, set2);
 
-    
+
 
 }
