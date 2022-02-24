d7e115da82c4721f7ef2da43c6c104776cc9a9cd
mvatanasov18 <mvatanasov18@codingburgas.bg>
2022-02-24 09:08:39 +0200

    Implement unionWith and display

--- 2021-mbp-07-Containers.cpp
+++ 2021-mbp-07-Containers.cpp
@@ -1,20 +1,40 @@
-// 2021-mbp-07-Containers.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <set>
+#include <iterator>
+#include <string>
+using namespace std;
+
+void display(set<string> set1)
+{
+    for (string s : set1)
+    {
+        cout << s << " ";
+    }
+}
+
+void unionWith(set<string> set1, set<string>set2)
+{
+    set<string> temp;
+    for (set<string>::iterator it = set1.begin(); it != set1.end(); it++)
+    {
+        temp.insert(*it);
+    }
+    for (set<string>::iterator it = set2.begin(); it != set2.end(); it++)
+    {
+        if (temp.find(*it) == temp.end())
+        {
+            temp.insert(*it);
+        }
+    }
+    display(temp);
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    set<string>set1 = {"Ivan","Kaloqn","Kiril","Mario"};
+    set<string>set2 = { "Martin", "Mario", "Petar","Kiril" };
+    unionWith(set1, set2);
+
+    
+
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
